c2f40c36e083c5ef28612b7e94b1eac4baffde65
Araceli Arizmendi <araceli.muse@gmail.com>
2020-10-20 14:07:42 -0700

    Initial Commit

---
+++ ProyectoParcial2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProyectoParcial2
+//
+//  Created by Alumno on 10/20/20.
+//  Copyright © 2020 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
